504f8cba96f696af1f35b6d9f6cf775ad97ffc52
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-06-05 20:26:11 -0500

    add api servic

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.1.
 
+## Goal
+
+1. Utilize NgRx SignalStore
+2. Define boundaries between Observables and Signals
+
 ## Exercise
 
 ### Steps
@@ -22,8 +27,6 @@ npm install @ngrx/signals@18.0.0-beta.1
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-### Build
+### Mock API
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-### Running unit tests
+Run `npm run serve-server` to start a mock API. `http://localhost:3000/contacts` will return a list of plenty of contacts. `ContactDataService` implements methods to interact with the API. 
--- src/app/contact/contacts.store.ts
+++ src/app/contact/contacts.store.ts
@@ -1,6 +1,9 @@
 import {patchState, signalStore, withHooks, withState} from '@ngrx/signals';
 import { Contact } from './contact.model';
 import {addEntity, withEntities} from "@ngrx/signals/entities";
+import {inject, Injectable} from "@angular/core";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, of} from "rxjs";
 
 export const dummy_contact: Contact = {id: "1", company: "Some Company", email: "some@company.com", name: "Some Name", photo: "https://flowbite.com/docs/images/people/profile-picture-3.jpg"}
 
@@ -13,3 +16,42 @@ export const ContactsStore = signalStore(
     }
   })
 );
+
+@Injectable({providedIn: "root"})
+export class ContactDataService {
+
+  private http = inject(HttpClient);
+
+  public getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>("http://localhost:3000/contacts").pipe(
+      catchError((error) => {
+        console.error(error);
+        return of([]);
+      })
+    )
+  }
+
+  public createContact(_contact: Pick<Contact, "name" | "company" | "email">): Observable<Contact | undefined> {
+    const contact = {
+      "age": 23,
+      "gender": "male",
+      "photo": "https://randomuser.me/api/portraits/men/50.jpg",
+      ..._contact
+    };
+    return this.http.post<Contact>("http://localhost:3000/contacts", contact).pipe(
+      catchError((error) => {
+        console.error(error);
+        return of(undefined);
+      })
+    )
+  }
+
+  public updateContact(contact: Contact): Observable<Contact> {
+    return this.http.patch<Contact>("http://localhost:3000/contacts/"+contact.id, contact).pipe(
+      catchError((error) => {
+        console.error(error);
+        return of(contact);
+      })
+    )
+  }
+}
